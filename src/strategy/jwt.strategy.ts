aa24c5445bca11bd0661d23ac0538922b6cd3ae8
thesunnatillo <sunnatillocoder@gmail.com>
2024-10-20 21:00:25 +0500

    update

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('AT-SECRET'),
+      secretOrKey: configService.get<string>('AT_SECRET'),
     });
   }
 
